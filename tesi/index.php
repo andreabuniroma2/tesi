f77a9fa3b26c3c6ec390ea85cbce95ccaabecad6
Andrea Blangiardo <andreablangiardo@gmail.com>
2021-07-07 20:37:55 +0200

    4

--- tesi/index.php
+++ tesi/index.php
@@ -4,7 +4,7 @@
 <head>
   <link rel="stylesheet" type="text/css" href="css/indexStyle.css">
   <link rel="stylesheet" type="text/css" href="css/navbar_content.css">
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/slider.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <title>Add Map</title>
   <script src="javascript/mappa.js"></script>
@@ -13,8 +13,42 @@
 <body>
   <?php require("header.php");
   ?>
-  <h3>My Google Maps Demo</h3>
-  <div id="map"></div>
+  <div class="container-fluid bordo">
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+        <div class="middle">
+
+          <div class="blocco_slider">
+            <div class="multi-range-slider">
+              <input type="range" id="input-left" min="1200" max="4000" value="1200" onchange="change(strumento)">
+              <input type="range" id="input-right" min="1200" max="4000" value="4000" onchange="change(strumento)">
+              <div class="slider">
+                <div class="track"></div>
+                <div class="range"></div>
+                <div class="thumb left"></div>
+                <div class="thumb right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6 col-md-6">
+        <div id="map"></div>
+      </div>
+      <div class="col-xs-6 col-md-3">lista incendi</div>
+
+    </div>
+  </div>
+  <div class="container-fluid no-gutters" id="lanostrastoria">
+        <h3>la nostra storia</h3>
+        <div class="storia">
+            <img src="immagini/vigile.jpeg" alt="no_image">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut excepturi ratione fugiat voluptates quisquam autem soluta modi, est ad magnam cum quo nobis vero voluptas odit quae ullam! Necessitatibus, atque.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut excepturi ratione fugiat voluptates quisquam autem soluta modi, est ad magnam cum quo nobis vero voluptas odit quae ullam! Necessitatibus, atque.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut excepturi ratione fugiat voluptates quisquam autem soluta modi, est ad magnam cum quo nobis vero voluptas odit quae ullam! Necessitatibus, atque.
+            </p>
+        </div>
+    </div>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCoJlELjVyZrWhf4_oUrYNkmz7Q-kCDToQ&callback=initMap&libraries=&v=weekly" async></script>
   <script src="javascript/geolocalisation.js"></script>
 </body>
